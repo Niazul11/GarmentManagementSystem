5042010e813be1656df38684e7b56eebc0d9dd1b
Niazul11 <naim23105101360@diu.edu.bd>
2024-10-31 17:32:08 +0600

    Added garment management classes

--- src/main/java/com/mycompany/garmentmanagementsystem/GarmentManagementSystem.java
+++ src/main/java/com/mycompany/garmentmanagementsystem/GarmentManagementSystem.java
@@ -1,16 +1,102 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-package com.mycompany.garmentmanagementsystem;
+import java.util.Date;
+import java.util.List;
 
-/**
- *
- * @author NiAjuL
- */
-public class GarmentManagementSystem {
+class Garment {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    String id;
+    String name;
+
+    String description;
+    String size;
+    String color;
+    double price;
+    int stockQuantity;
+
+    void updateStock(int quantity) {
+
     }
+
+    double calculateDiscountPrice(double discountPercentage) {
+        return 0;
+    }
+}
+
+class Fabric {
+
+    String id;
+    String type;
+    String color;
+    double pricePerMeter;
+
+    double calculateCost(double meters) {
+        return 0;
+    }
+
+}
+
+class Supplier {
+
+    String id;
+    String name;
+    String contactInfo;
+    List<Fabric> suppliedFabrics;
+
+    void addFabric(Fabric fabric) {
+
+    }
+
+    List<Fabric> getSuppliedFabrics() {
+        return null;
+    }
+}
+
+class Order {
+
+    String orderId;
+    Date orderDate;
+    List<Garment> garments;
+    double totalAmount;
+
+    void addGarment(Garment garment) {
+    }
+
+    double calculateTotalAmount() {
+        return 0;
+    }
+
+    void printOrderDetails() {
+    }
+}
+
+class Customer {
+
+    String customerId;
+    String name;
+    String email;
+    String phone;
+
+    void placeOrder(Order order) {
+    }
+
+    List<Order> viewOrders() {
+        return null;
+    }
+
+}
+
+class Inventory {
+
+    List<Garment> garments;
+
+    void addGarment(Garment garment) {
+    }
+
+    void removeGarment(String id) {
+    }
+
+    Garment findGarment(String id) {
+        return null;
+    }
+
 }
