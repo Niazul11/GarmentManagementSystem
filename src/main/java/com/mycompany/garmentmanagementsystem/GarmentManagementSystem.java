95ca5b78c91df7eef46eed9f1ba0be301fd3b33f
Niazul11 <naim23105101360@diu.edu.bd>
2024-10-31 17:17:14 +0600

    Initial commit: Add garment management system classes

---
+++ src/main/java/com/mycompany/garmentmanagementsystem/GarmentManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.garmentmanagementsystem;
+
+/**
+ *
+ * @author NiAjuL
+ */
+public class GarmentManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
